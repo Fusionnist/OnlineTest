b93e0b1a014a22efac997dca231b42c0a600bd23
Fusionnist <antoine.chartier@gmail.com>
2017-06-22 17:51:16 +0200

    wow

--- Template/Game1.cs
+++ Template/Game1.cs
@@ -8,18 +8,26 @@ namespace Template
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        Vector2 pos;
+        Texture2D tex;
         
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
+            graphics.PreferredBackBufferHeight = 600;
+            graphics.PreferredBackBufferWidth = 600;
+            graphics.ApplyChanges();
+
             Content.RootDirectory = "Content";
         }
         protected override void Initialize()
         {
+            pos = new Vector2(300, 300);
             base.Initialize();
         }
         protected override void LoadContent()
         {
+            tex = Content.Load<Texture2D>("ship");
             spriteBatch = new SpriteBatch(GraphicsDevice);
         }
         protected override void UnloadContent()
@@ -31,10 +39,18 @@ namespace Template
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
             base.Update(gameTime);
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Down)) { pos.Y += 3; }
+            if (Keyboard.GetState().IsKeyDown(Keys.Up)) { pos.Y -= 3; }
+            if (Keyboard.GetState().IsKeyDown(Keys.Left)) { pos.X -= 3; }
+            if (Keyboard.GetState().IsKeyDown(Keys.Right)) { pos.X += 3; }
         }
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+            spriteBatch.Begin();
+            spriteBatch.Draw(tex, pos, null);
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
