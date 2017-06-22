cfaba2b33fb720c0286059f0ae9ca72e1f6fd29e
Louis Vassaux <louis.vassaux@sfr.fr>
2017-06-22 17:50:48 +0200

    sprite

---
+++ Template/Sprite.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Template
+{
+    class Sprite
+    {
+        Texture2D tex;
+        Vector2 pos;
+
+        public Sprite(Texture2D a_tex, Vector2 a_pos)
+        {
+            tex = a_tex;
+            pos = a_pos;
+        }
+
+        public void Draw(SpriteBatch a_sb)
+        {
+            a_sb.Draw(tex, pos);
+        }
+    }
+}
