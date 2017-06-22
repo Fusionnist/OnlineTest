3979dc60c72faac356a7adfeb345898658b498b5
Imnibis <imnibis@outlook.fr>
2017-06-22 17:45:29 +0200

    Added server class

---
+++ Template/Server.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quobject.SocketIoClientDotNet.Client;
+
+namespace Template
+{
+    class Server
+    {
+        Socket socket = null;
+
+        public Server(String host)
+        {
+            socket = IO.Socket(host);
+        }
+
+        public void setSocket(Socket socket)
+        {
+            this.socket = socket;
+        }
+
+        public Socket getSocket()
+        {
+            return socket;
+        }
+    }
+}
