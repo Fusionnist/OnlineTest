0d2569e4c438a9969fbfcbf1b7c8f303c44fa50e
Fusionnist <antoine.chartier@gmail.com>
2017-06-26 14:45:53 +0200

    wow

--- Template/Game1.cs
+++ Template/Game1.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using Lidgren;
+using Lidgren.Network;
 
 namespace Template
 {
@@ -9,6 +12,10 @@ namespace Template
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         Sprite ship;
+        RenderTarget2D InterfaceTarget, GameTarget;
+        Vector2 translation;
+        NetPeerConfiguration peerConfig;
+        NetPeer peer;
         
         public Game1()
         {
@@ -18,11 +25,29 @@ namespace Template
             graphics.ApplyChanges();
 
             Content.RootDirectory = "Content";
+
+            peerConfig = new NetPeerConfiguration("TestGame");
+            peerConfig.Port = 8000;
+            peer = new NetPeer(peerConfig);
+            peer.Start();
+        }
+        void CalcTranslation()
+        {
+            translation = ship.pos - new Vector2(284, 284);
         }
         protected override void Initialize()
         {
-            ship = new Sprite(Content.Load<Texture2D>("ship"), new Vector2(300, 300));
+            InterfaceTarget = new RenderTarget2D(GraphicsDevice, 600, 600);
+            GameTarget = new RenderTarget2D(GraphicsDevice, 600, 600);
+            ship = new Sprite(Content.Load<Texture2D>("ship"), new Vector2(0, 0));
             base.Initialize();
+        }
+        void HandleWebConnections()
+        {
+        }
+        void UpdateWebConnections()
+        {
+
         }
         protected override void LoadContent()
         {
@@ -47,9 +72,27 @@ namespace Template
         }
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-            spriteBatch.Begin();
+            GraphicsDevice.SetRenderTarget(GameTarget);
+            float x = Math.Abs(ship.pos.X / 1000);
+            float y = Math.Abs(ship.pos.Y / 1000);
+            Color c = new Color(x, y, x + y/2);
+            GraphicsDevice.Clear(c);
+            CalcTranslation();
+            Matrix m = Matrix.CreateTranslation(new Vector3(-translation.X, -translation.Y, 0)) * Matrix.CreateScale(1f);
+            spriteBatch.Begin(transformMatrix: m);
             ship.Draw(spriteBatch);
+            spriteBatch.End();
+
+            GraphicsDevice.SetRenderTarget(InterfaceTarget);
+            GraphicsDevice.Clear(Color.TransparentBlack);
+            spriteBatch.Begin();
+            //draw interface
+            spriteBatch.End();
+
+            GraphicsDevice.SetRenderTarget(null);
+            spriteBatch.Begin();
+            spriteBatch.Draw(texture: GameTarget, position: Vector2.Zero);
+            spriteBatch.Draw(texture: InterfaceTarget, position: Vector2.Zero);
             spriteBatch.End();
 
             base.Draw(gameTime);
